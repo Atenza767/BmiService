be158196561185b26b119b8c15d6a5d465828b5e
Atenza767 <atenza767@gmail.com>
2026-02-05 19:55:06 +0100

    change 1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,7 @@
-public class calculate {
-
+public class Calculator {
     public int calculate(int weight, double height) {
         double resultBmi;
-        {
         resultBmi = weight / (height * height);
-        }
     return (int)resultBmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        calculate service = new calculate();
+        Calculator service = new Calculator();
         int weight = 98;
         double height = 1.87;
         int result = service.calculate(weight, height);
         System.out.println((result) + " индекс массы тела");
-
     }
 }
