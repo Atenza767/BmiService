15fb112584b45d52f2fdb5ba98bfa9acb4503fe1
Atenza767 <atenza767@gmail.com>
2026-02-09 20:39:19 +0100

    change 2

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
     public int calculate(int weight, double height) {
         double resultBmi;
         resultBmi = weight / (height * height);
-    return (int)resultBmi;
+        return (int) resultBmi;
     }
 }
